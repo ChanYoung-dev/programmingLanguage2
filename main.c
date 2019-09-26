ba1af079c723c29d12c787bb5188e791ba6d80d5
emrhssla <emrhssla@gmail.com>
2019-09-26 10:24:15 +0900

    포인터 증가연산

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 int main(void)
 {
-    int i=10;
-    int *p;
+    char *pc;
+    int *pi;
+    double *pd;
 
-    p = &i;
-    printf("i = %d\n", i);
+    pc = (char *)10000;
+    pi = (int *)10000;
+    pd = (double *)10000;
+    printf("증가 전 pc = %d,  pi = %d,  pd = %d\n", pc, pi, pd);
 
-    *p = 20;
-    printf("i = %d\n", i);
+    pc++;
+    pi++;
+    pd++;
+    printf("증가 후 pc = %d,  pi = %d,  pd = %d\n", pc, pi, pd);
+    printf("pc+2 = %d, pi+2 = %d, pd+2 = %d\n", pc+2, pi+2, pd+2);
     return 0;
 }
