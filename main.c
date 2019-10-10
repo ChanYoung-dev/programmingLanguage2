44721ef49312d45bca27fdcee59e8479c26de59c
emrhssla <emrhssla@gmail.com>
2019-10-10 10:00:23 +0900

    포인터와 배열

--- main.c
+++ main.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
+
 int main(void)
 {
-    char *pc;
-    int *pi;
-    double *pd;
-
-    pc = (char *)10000;
-    pi = (int *)10000;
-    pd = (double *)10000;
-    printf("증가 전 pc = %d,  pi = %d,  pd = %d\n", pc, pi, pd);
-
-    pc++;
-    pi++;
-    pd++;
-    printf("증가 후 pc = %d,  pi = %d,  pd = %d\n", pc, pi, pd);
-    printf("pc+2 = %d, pi+2 = %d, pd+2 = %d\n", pc+2, pi+2, pd+2);
+    int a[] = { 10, 20, 30, 40, 50 };
+    printf("a = %u\n", a);
+    printf("a + 1 = %u\n", a + 1);
+    printf("*a = %d\n", *a);
+    printf("*(a+1) = %d\n", *(a+1));
     return 0;
 }
