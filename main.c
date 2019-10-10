583bc22aa3f541f72a6ebddd00a169265204f059
emrhssla <emrhssla@gmail.com>
2019-10-10 10:07:01 +0900

    포인터를이용한 배열의 합

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+int array_sum(int *A, int size){
+    int i, sum=0;
+    for(i=0 ; i<size ; i++){
+        sum+=*(A++);
+    }
+    return sum;
+
+}
 
 int main(void)
 {
     int a[] = { 10, 20, 30, 40, 50 };
-    printf("a = %u\n", a);
-    printf("a + 1 = %u\n", a + 1);
-    printf("*a = %d\n", *a);
-    printf("*(a+1) = %d\n", *(a+1));
+    printf("%d",array_sum(a,5));
     return 0;
 }
