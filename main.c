7c7cdf3ea49053638fbe3da921d27d4de16e0ac0
emrhssla <emrhssla@gmail.com>
2019-10-24 09:19:01 +0900

    문자열안에서의 숫자 검색

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int main(void)
 {
-    char src[] = "The worst things to eat before you sleep";
-    char dst[100];
-    int i;
-    printf("원본 문자열=%s\n", src);
-    for(i=0 ; src[i] != '\0' ; i++)
-        dst[i] = src[i];
-    dst[i] = '\0';
-    printf("복사된 문자열=%s\n", dst);
+    // 크기가 50인 char형 배열선언
+    char str[50];
+    //반복 변수 선언
+    int i=0;
+    //문자열 입력
+    printf("문자열을 입력하세요: ");
+    scanf("%s", str);
+    //저장한 문자열 출력
+    printf("%s\n", str);
+    //숫자 검색
+    for(i=0;i<strlen(str);i++) {
+        if (str[i] >= '0' && str[i] <= '9')
+            printf("%c", str[i]);  // 숫자 출력
+    }
     return 0;
 }
