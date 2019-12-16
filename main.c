d7ad8be9e70d1d663d4169b4110eb32f516809e8
emrhssla <emrhssla@gmail.com>
2019-12-16 23:50:37 +0900

    14-1

--- main.c
+++ main.c
@@ -1,74 +1,35 @@
 #include <stdio.h>
-#define SIZE 4
-void merge(int *A, int *B, int *C, int size){
-    int i, A_state=0, B_state=0;
-    //C의 배열 크기만큼 반복
-    for(i=0; i<size*2; i++){
-        // A배열과 B배열, 두 배열 중 어느 하나도 배열값이 전부 들어가지 않은 경우
-        if(A_state<size && B_state<size){
-            //현재A배열 원소값과 현재B배열 원소값 비교하여 A배열값이 더 작은경우
-            if(A[A_state]<=B[B_state]){
-                //현재A배열 원소값을 현재 C배열 원소에 삽입
-                C[i]=A[A_state];
-                //A배열의 현재상태 원소번호를 증가
-                A_state++;
-            }
-            //현재A배열 원소값과 현재B배열 원소값 비교하여 B배열값이 더 작은경우
-            else if(A[A_state]>B[B_state]){
-                //현재B배열 원소값을 현재 C배열 원소에 삽입
-                C[i]=B[B_state];
-                //B배열의 현재상태 원소번호를 증가
-                B_state++;
-            }
-        }
-        //A배열의 현재상태 원소번호가 A배열의 끝을 초과한 경우
-        //즉, A배열이 C배열에 전부 들어갔을 경우
-        //B배열을 C배열 나머지에 넣어준다.
-        else if(A_state==size){
-            //현재상태 B배열 원소번호에 있는 원소을 C배열의 나머지 원소에 넣어준다.
-            C[i]=B[B_state];
-            //B배열의 현재상태 원소번호를 증가시킨다.
-            B_state++;
-        }
-        //B배열의 현재상태 원소번호가 B배열의 끝을 초과한 경우
-        //즉, B배열이 C배열에 전부 들어갔을 경우
-        //A배열을 C배열 나머지에 넣어준다.
-        else if(B_state==size){
-            //현재상태 A배열 원소번호에 있는 원소을 C배열의 나머지 원소에 넣어준다.
-            C[i]=A[A_state];
-            //A배열의 현재상태 원소번호를 증가시킨다.
-            A_state++;
-        }
-        //A배열과 B배열의 현재 원소 번호 값이 각 배열의 끝을 초과한 경우
-        //A배열과 B배열의 원소들이 다 들어간경우 탈출한다.
-    }
+#include <string.h>
+//이중포인터 q를 통하여 외부의 포인터 p를 변경
+//함수 호출시에 인수들은 복사본이 전달되기때문에 p자체를 변경하려면 p의 주소를 변경해야한다.
+void set_proverb(char **q, int n) {
+    static char *array[10] = {"A bad shearer never had a good sickle",
+                              "A bad workman (always) blames his tools",
+                              "A bad workman quarrels with his tools",
+                              "A bad thing never dies",
+                              "The grass is always greener on the other side of the fence.",
+                              "Don’t judge a book by its cover.",
+                              " Strike while the iron is hot.",
+                              "Too many cooks spoil the broth",
+                              "You can’t have your cake and eat it too.",
+                              "Many hands make light work"
+    };
+    //배열은 0원소번호부터 시작하지만 사용자는 n번째를 선택했으니 n-1을 해준다.
+    *q = array[n-1];
 }
-//배열 출력
-void arrange_print(int *A, int size){
-    int i;
-    for(i=0; i<size; i++){
-        //배열의 끝원소출력
-        if (i==size-1)	printf("%d \n", A[i]);
-        //마지막을 제외한 배열 출력
-        else 			printf("%d ", A[i]);
-    }
-}
+int main() {
 
-void main(){
-    int A[SIZE] = {2, 5, 7, 8};
-    int B[SIZE] = {1, 3, 4, 6};
-    int C[SIZE*2]={0};
-    // A와 B 배열들을 합친다.
-    merge(A, B, C, SIZE);
+    //문자열의 주소를 저장하는 포인터 p
+    char *p;
+    //사용자가 원하는 n번째 속담
+    int n;
+    printf("몇 번째 속담을 선택하시겠습니까?  ");
+    scanf("%d", &n);
+    //포인터 p의 주소를 전달한다.
+    set_proverb(&p, n);
+    //문자열을 출력하기 위해 문자열의 주소(p)입력
+    printf("selected proverb = %s \n", p);
 
-    printf("A[] = ");
-    arrange_print(A, SIZE);
-
-    printf("B[] = ");
-    arrange_print(B, SIZE);
-
-    printf("C[] = ");
-    arrange_print(C, SIZE*2);
-
+    return 0;
 }
 
