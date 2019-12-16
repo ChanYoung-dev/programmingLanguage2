9266f3ec057f495e0b4bcad7b681ffcdc1e0d6b9
emrhssla <emrhssla@gmail.com>
2019-12-16 21:42:25 +0900

    11-4 programming

--- main.c
+++ main.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
-int main(void)
-{
-    FILE *fp = NULL;
-    int c;
-    fp = fopen("sample.txt", "r");
-    if( fp == NULL )
-        printf("파일 열기 실패\n");
-    else
-        printf("파일 열기 성공\n");
 
-    while((c = fgetc(fp)) != EOF )
-        putchar(c);
-    fclose(fp);
-    return 0;
+void array_print(int *A, int size){
+
+    int i;
+
+    printf("A[] = { ");
+
+    for(i=0; i<size; i++){
+
+        //배열의 마지막 원소일 경우
+        if(i==size-1)
+            //A의 현재배열 A[i]를 출력하고 i를 증가시킨다. A[i++]와 같다.
+            printf("%d", *A++);
+        //배열의 마지막 원소를 제외한 원소인 경우
+        else
+            // 처음 A는 A[0]의 주소이다.
+            // *A++로 인해 A[0]에 있는 값을 출력시키고 A[0]에서 A[1]이 된다.
+            // 이렇게 반복한다.
+            // A의 현재배열 A[i]를 출력하고 i를 증가시킨다. A[i++]와 같다.
+            printf("%d, ", *A++);
+
+    }
+
+    printf(" }\n");
+
+}
+
+
+
+void main(){
+
+    int A[5]={1,2,3,4,5};
+    //size를 절대값으로 정해놨을경우
+    //array_print(A, 5);
+
+    //배열의 크기를 유동적으로 바꿀수 있게 코드 수정
+    array_print(A, sizeof(A)/sizeof(A[0]));
 }
