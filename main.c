5e64676052ba7e4c5f43e04859148f77430d8503
emrhssla <emrhssla@gmail.com>
2019-12-18 22:46:12 +0900

    697

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 struct student{
     char name[10];
@@ -9,20 +9,44 @@ struct student{
 };
 
 int main(){
-    struct student list[]={
-            {"chan",20131000,99},
-            {"seong",20122000,84},
-            {"minsoo",2011111,23}
-    };
 
-
-    int max_score,i;
-    max_score=list[0].score;
-    for(i=1;i<(sizeof(list)/sizeof(list[0]));i++){
-        if(max_score<list[i].score)
-            max_score=list[i].score;
-
+    int i;
+    struct student list[3];
+    FILE *fp=NULL;
+    FILE *fp2=NULL;
+    FILE *fp3=NULL;
+    fp=fopen("studentread.txt","w");
+    fp2=fopen("studentread.txt","r");
+    fp3=fopen("studentread1.txt","w");
+    char buffer1[100];
+    if(fp==NULL){
+        printf("오류입니다.");
     }
-    printf("%d",max_score);
-    return 0;
+    else
+        printf("성공\n");
+
+
+    fprintf(fp,"학번 이름 점수\n");
+    for(i=0;i<3;i++) {
+        printf("학번 입력 : ");
+        scanf("%d", &list[i].number);
+        if(list[i].number<0)
+        {
+            break;
+        }
+        printf("이름 점수 입력 : ");
+        scanf("%s %d", list[i].name, &list[i].score);
+        fprintf(fp,"%d %s %d\n",list[i].number,list[i].name,list[i].score);
+    }
+
+
+    fgets(buffer1,100,fp2);
+
+    for(i=0;i<3;i++) {
+        fscanf(fp2, "%d %s %d", &list[i].number, list[i].name, &list[i].score);
+        fprintf(fp3,"%s %d",list[i].name,list[i].score+1);
+    }
+    fclose(fp);
+    fclose(fp2);
+    fclose(fp3);
 }
