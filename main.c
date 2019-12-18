e1f4535e1456b3c09c0ce48034eda4baf688a0c9
emrhssla <emrhssla@gmail.com>
2019-12-18 23:07:58 +0900

    747

--- main.c
+++ main.c
@@ -2,51 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct student{
-    char name[10];
-    int number;
-    int score;
-};
 
 int main(){
 
     int i;
-    struct student list[3];
-    FILE *fp=NULL;
-    FILE *fp2=NULL;
-    FILE *fp3=NULL;
-    fp=fopen("studentread.txt","w");
-    fp2=fopen("studentread.txt","r");
-    fp3=fopen("studentread1.txt","w");
-    char buffer1[100];
-    if(fp==NULL){
-        printf("오류입니다.");
-    }
-    else
-        printf("성공\n");
+    int *link=(int*)malloc(2*sizeof(int));
+    printf("점수 입력하시오:");
 
-
-    fprintf(fp,"학번 이름 점수\n");
-    for(i=0;i<3;i++) {
-        printf("학번 입력 : ");
-        scanf("%d", &list[i].number);
-        if(list[i].number<0)
-        {
-            break;
-        }
-        printf("이름 점수 입력 : ");
-        scanf("%s %d", list[i].name, &list[i].score);
-        fprintf(fp,"%d %s %d\n",list[i].number,list[i].name,list[i].score);
+    for(i=0;i<2;i++) {
+        printf("학생%d점수입력", i + 1);
+        scanf("%d", &link[i]);
     }
 
+    printf("--------------------");
+    for(i=0;i<2;i++) {
+        printf("학생%d:%d\n",i+1, link[i]);
 
-    fgets(buffer1,100,fp2);
-
-    for(i=0;i<3;i++) {
-        fscanf(fp2, "%d %s %d", &list[i].number, list[i].name, &list[i].score);
-        fprintf(fp3,"%s %d",list[i].name,list[i].score+1);
     }
-    fclose(fp);
-    fclose(fp2);
-    fclose(fp3);
+    free(link);
+    return 0;
 }
